e8481867b6e5ec6d7b19db49e67f67ef564a7667
samueldamasceno <samueldamasceno.vip@gmail.com>
2024-09-20 11:02:13 -0300

    - Escolha de uma api pública
     - Requisição de acordo com a documentação da API
    
     - Geração do arquivo Json com os dados.+

---
+++ src/main/java/com/fiap/coquetelapi/coquetel_api_cliente/CoquetelApiClienteApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.coquetelapi.coquetel_api_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoquetelApiClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoquetelApiClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/coquetelapi/coquetel_api_cliente/CoquetelApiClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.coquetelapi.coquetel_api_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoquetelApiClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
